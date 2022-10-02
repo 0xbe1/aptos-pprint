4ffccacd78eef2566ab50b6e3f8de01e47328553
0xbe1 <0xbetrue@gmail.com>
2022-10-02 20:36:49 +0800

    feat(#2): use regex to match u8 array

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Before 🤯
 $ aptos move test
 ...
 [debug] (&) [97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121]
+[debug] (&) { [97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121] }
 ```
 
 After 😇
@@ -15,7 +16,8 @@ After 😇
 ```
 $ aptos move test | aptos-pprint
 ...
-[debug] aptos debug made easy
+[debug] (&) aptos debug made easy
+[debug] (&) { aptos debug made easy }
 ```
 
 ## Install
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,83 @@
+use regex::{Captures, Regex};
 use std::io;
 use std::io::BufRead;
 
 const DEBUG_PREFIX: &str = "[debug] (&)";
+
+#[derive(Debug, Clone, PartialEq)]
+struct ParseError {
+    msg: String,
+}
 
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from stdin");
         if line.starts_with(DEBUG_PREFIX) {
-            println!("[debug] {}", parse_debug_print(line))
+            println!("{}", pprint(line))
         } else {
             println!("{}", line);
         }
     }
 }
 
-fn parse_debug_print(line: String) -> String {
-    let numbers = &line[DEBUG_PREFIX.len() + 2..line.len() - 1];
-    let numbers: Vec<u8> = numbers.split(", ").map(|s| s.parse().unwrap()).collect();
-    String::from_utf8(numbers).unwrap()
+fn pprint(line: String) -> String {
+    let re = Regex::new(r"\[(\d+,\s)+\d+]").unwrap();
+    let result = re.replace(line.as_str(), |caps: &Captures| {
+        let s = caps.get(0).map_or("", |m| m.as_str());
+        parse_utf8_arr(s).unwrap_or(s.to_string())
+    });
+    format!("{}", result)
+}
+
+fn parse_utf8_arr(s: &str) -> Result<String, ParseError> {
+    let s_arr: Vec<&str> = s[1..s.len() - 1].split(", ").collect();
+    let u8_arr = s_arr
+        .iter()
+        .filter_map(|ss| ss.parse().ok())
+        .collect::<Vec<u8>>();
+    if s_arr.len() > u8_arr.len() {
+        Err(ParseError {
+            msg: "not an u8 array".to_string(),
+        })
+    } else {
+        String::from_utf8(u8_arr).or(Err(ParseError {
+            msg: "u8 array => str nok".to_string(),
+        }))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::parse_debug_print;
+    use crate::{parse_utf8_arr, pprint, ParseError};
+
+    #[test]
+    fn pprint_ok() {
+        assert_eq!(
+            pprint("[debug] (&) [97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121]".to_string()),
+            "[debug] (&) aptos debug made easy"
+        );
+        assert_eq!(
+            pprint("[debug] (&) { [97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121] }".to_string()),
+            "[debug] (&) { aptos debug made easy }"
+        );
+        assert_eq!(
+            pprint("[debug] (&) [0, 300]".to_string()),
+            "[debug] (&) [0, 300]"
+        );
+    }
 
     #[test]
     fn parse_ok() {
-        assert_eq!(parse_debug_print("[debug] (&) [97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121]".to_string()), "aptos debug made easy");
+        assert_eq!(
+            parse_utf8_arr("[97, 112, 116, 111, 115, 32, 100, 101, 98, 117, 103, 32, 109, 97, 100, 101, 32, 101, 97, 115, 121]"),
+            Ok("aptos debug made easy".to_string())
+        );
+        assert_eq!(
+            parse_utf8_arr("[0, 300]"),
+            Err(ParseError {
+                msg: "not an u8 array".to_string()
+            })
+        );
     }
 }
